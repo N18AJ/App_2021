3af8d42c62362ca0fd1a59ad725186dba50ad899
N18AJ <nealju96@gmail.com>
2021-02-01 11:02:34 +0100

    2021/02/01
    Ficheros REST

--- controller/cREST.php
+++ controller/cREST.php
@@ -10,18 +10,26 @@ if (isset($_REQUEST['Volver'])) {
     header('Location: index.php');
     exit;
 }
-if(isset($_REQUEST['Enviar'])) { //si se ha enviado una fecha
+
+
+$apodSelected = "selected"; //marcamos como seleccionado el servicio por defecto (apod)
+$apodDisplay = "block"; //mostramos el servicio por defecto (apod)
+error_reporting(0);
+if(isset($_REQUEST['enviar'])) { //si se ha enviado una fecha (Hecho por Rodrigo)
     //llamamos al servicio y le pasamos la fecha introducida por el usuario
     $aServicioAPOD = REST::sevicioAPOD($_REQUEST['fecha']);
 }
-else {
+else { //si no
     //llamamos al servicio y le pasamos la fecha de hoy
     $aServicioAPOD = REST::sevicioAPOD(date('Y-m-d'));
 }
-$tituloEnCurso = $aServicioAPOD['title'];
-$imagenEnCurso = $aServicioAPOD['url'];
-$descripcionEnCurso = $aServicioAPOD['explanation'];
+error_reporting(-1);
 
+if(isset($error)) { //si ha ocurrido algún error
+    //guardamos el mensaje y la imagen de error para pasárselos a la vista
+    $tituloEnCurso = "ERROR: ".$error;
+    $imagenEnCurso = "webroot/icons/sorry.jpg";
+}
 
 
 if (isset($_REQUEST['Aceptar'])) { //si se ha enviado una fecha
@@ -33,27 +41,23 @@ if (isset($_REQUEST['Aceptar'])) { //si se ha enviado una fecha
 if (is_null($ValoresActor)) {
     $nombrePersonaje = "Sin petición";
     $cumpleaños = null;
-    $ocupacion = null;
     $imagen = null;
     $estado = null;
     $apodo = null;
-    $aparicion = null;
     $actor = null;
     $categoria = null;
     
 } else {
-    $nombrePersonaje = $ValoresActor['name'];
-    $cumpleaños = $ValoresActor['birthday'];
-    $ocupacion = $ValoresActor['occupation'];
-    $imagen = $ValoresActor['img'];
-    $estado = $ValoresActor['status'];
-    $apodo = $ValoresActor['nickname'];
-    $aparicion = $ValoresActor['appearance'];
-    $actor = $ValoresActor['portrayed'];
-    $categoria = $ValoresActor['category'];
+    $nombrePersonaje = $ValoresActor[0]['name'];
+    $cumpleaños = $ValoresActor[0]['birthday'];
+    $imagen = $ValoresActor[0]['img'];
+    $estado = $ValoresActor[0]['status'];
+    $apodo = $ValoresActor[0]['nickname'];
+    $actor = $ValoresActor[0]['portrayed'];
+    $categoria = $ValoresActor[0]['category'];
 }
 
-var_dump($ValoresActor);
+//var_dump($ValoresActor);
 
 $vistaEnCurso = $vistas['restPag']; //variable que contiene la vista que va a ejecutarse
 require_once $vistas['layout']; //llamamos al layout
--- view/vREST.php
+++ view/vREST.php
@@ -10,24 +10,33 @@
         <section>
             <div id="cuerpo-rest">
                 <p id="titulo-rest">Uso de API REST</p>
-                <div id="formulario-rest">
-                    <p>APOD: Atronomy Picture of the Day</p>
+                 <div id="formulario-rest">
+                <div id="apodForm" style="display: <?php echo $apodDisplay ?>">
                     <p>Puedes seleccionar una fecha para ver su imagen</p>
-                    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                    <form name="rest" id="rest" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
                         <p>
-                            <input type="date" name="fecha" value="<?php echo date('Y-m-d') ?>">
+                            <input type="date" name="fecha" max="<?php echo date('Y-m-d') ?>" value="<?php echo date('Y-m-d') ?>">
                         </p>
                         <div class="botones">
                             <input type="submit" value="Enviar" name="enviar">
                         </div>
                     </form>
                 </div>
-                <div id="servicio-rest">
-                    <p><?php echo $tituloEnCurso?></p>
-                    <img src="<?php echo $imagenEnCurso?>" width="100">
-                    <p><?php echo $descripcionEnCurso?></p>
-                </div>
             </div>
+            <div id="servicio-rest">
+                <div id="apodService" style="display: <?php echo $apodDisplay ?>">
+<!--                    <p>Titulo APOD</p>
+                    <img src="webroot/icons/perro.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta nisi ut tristique tempor. Nulla facilisi. Ut tempus luctus tincidunt. Curabitur cursus diam enim, ac tempor odio congue vitae. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a ipsum enim. Sed accumsan, sem ut mattis lobortis, lorem nulla convallis velit, sit amet suscipit turpis sapien quis risus. Cras pellentesque, quam sit amet venenatis vestibulum, tellus nisl suscipit felis, quis finibus nunc odio vel elit. Praesent dictum vestibulum nunc et elementum. Pellentesque ut enim vitae risus pretium semper. Maecenas molestie aliquet nisi nec sodales.</p>-->
+                    <p><?php echo $aServicioAPOD['title'] ?></p>
+                    <img src="<?php echo $aServicioAPOD['url'] ?>" width="100">
+                    <p><?php echo $aServicioAPOD['explanation'] ?></p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <section class="barra-inferior" id="inferior-rest">
         </section>
             
     </article>
@@ -37,19 +46,17 @@
     <form style="text-align: center;" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <h2>Breaking Bad</h2>
         <div>
-            <p>Nombre del actor: <?php echo $nombrePersonaje?></p>
-            <p>Fecha cumpleaños: <?php echo $cumpleaños ?></p>
-            <p>Ocupaciones del personaje: <?php echo $ocupacion ?></p>
-            <p>Imagen: <?php echo $imagen ?></p>
-            <p>Estado del personaje: <?php echo $estado ?></p>
-            <p>Apodo del pesonaje: <?php echo $apodo ?></p>
-            <p>Temporadas de rodaje: <?php echo $aparicion ?></p>
-            <p>Actor: <?php echo $actor ?></p>
-            <p>Series: <?php echo $categoria ?></p>
+            <p style="font-size:16pt"><span style="font-weight:bold; font-size:20pt">Nombre del actor: </span> <?php echo $nombrePersonaje?></p>
+            <p style="font-size:16pt"><span style="font-weight:bold;">Fecha cumpleaños: </span> <?php echo $cumpleaños ?></p>
+            <p style="font-size:16pt"><span style="font-weight:bold;">Imagen: </span> <?php echo $imagen ?></p>
+            <p style="font-size:16pt"><span style="font-weight:bold;">Estado del personaje: </span> <?php echo $estado ?></p>
+            <p style="font-size:16pt"><span style="font-weight:bold;">Apodo del pesonaje: </span> <?php echo $apodo ?></p>
+            <p style="font-size:16pt"><span style="font-weight:bold;">Actor: </span> <?php echo $actor ?></p>
+            <p style="font-size:16pt"><span style="font-weight:bold;">Series: </span> <?php echo $categoria ?></p>
             
             
         </div>
-        <input type="number" placeholder="1-15" max="15" min="1" name="numero" />
+        <input type="number" placeholder="1-57" max="57" min="1" name="numero" />
         <div>
             <button class="button" type="submit" name="Aceptar"><?php echo $aLang[$_COOKIE['idioma']]['acceptB']; ?></button>
         </div>
